a4484153b8776ebb02b9590f108f5f809442b981
Oluwatobi Samuel Omisakin <omisakin.oluwatobi@gmail.com>
2020-03-11 20:10:47 +0200

    copy only needed files

--- setup.sh
+++ setup.sh
@@ -5,18 +5,12 @@ then
     if [ -d "../$project_path" ]
     then
         echo path exist $project_path
-        cp docker-compose.yml ../$project_path/
-        cp Dockerfile ../$project_path/
-        cp dockerize.sh ../$project_path/
-        cp .env.dockerizer.local ../$project_path/
+        cp -vRT src/ ../$project_path/
     fi
 fi
 
 if [ ! "$project_path"] & [ -f "../artisan" ]
 then
     echo "Artisan exists"
-    cp docker-compose.yml ../
-    cp Dockerfile ../
-    cp dockerize.sh ../
-    cp .env.dockerizer.local ../
+    cp -vRT src/ ../
 fi
